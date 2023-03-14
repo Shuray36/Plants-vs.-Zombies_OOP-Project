b4339dd95d5bb36d3f805e6c7a8a3fbef914f6e8
Shuray <aaa951159@gmail.com>
2023-03-14 15:51:22 +0800

    new 1.1.2

---
+++ LeistungsstarkesGameFramework-1.1.2/Source/Game/mygame_initialize.cpp
@@ -0,0 +1,96 @@
+#include "stdafx.h"
+#include "../Core/Resource.h"
+#include <mmsystem.h>
+#include <ddraw.h>
+#include "../Library/audio.h"
+#include "../Library/gameutil.h"
+#include "../Library/gamecore.h"
+#include "mygame.h"
+#include <string>
+
+using namespace game_framework;
+/////////////////////////////////////////////////////////////////////////////
+// 這個class為遊戲的遊戲開頭畫面物件
+/////////////////////////////////////////////////////////////////////////////
+
+CGameStateInit::CGameStateInit(CGame *g) : CGameState(g)
+{
+}
+
+void CGameStateInit::OnInit()
+{
+	//
+	// 當圖很多時，OnInit載入所有的圖要花很多時間。為避免玩遊戲的人
+	//     等的不耐煩，遊戲會出現「Loading ...」，顯示Loading的進度。
+	//
+	ShowInitProgress(0, "Start Initialize...");	// 一開始的loading進度為0%
+	Sleep(200);
+	//
+	// 開始載入資料
+	//
+	load_background();
+
+	ShowInitProgress(66, "Initialize...");
+	Sleep(200);
+	//Sleep(1000);				// 放慢，以便看清楚進度，實際遊戲請刪除此Sleep
+	//
+	// 此OnInit動作會接到CGameStaterRun::OnInit()，所以進度還沒到100%
+	//
+}
+
+void CGameStateInit::OnBeginState()
+{
+}
+
+void CGameStateInit::OnKeyUp(UINT nChar, UINT nRepCnt, UINT nFlags)
+{
+
+}
+
+void CGameStateInit::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	if (pointx >= 476 && pointx <= 797 && pointy >= 80 && pointy <= 188) {
+		GotoGameState(GAME_STATE_RUN);
+	}
+}
+
+void CGameStateInit::OnMouseMove(UINT nFlags, CPoint point)
+{
+	pointx = point.x;
+	pointy = point.y;
+}
+
+
+void CGameStateInit::OnShow()
+{
+	background.ShowBitmap();
+	start_button_1.ShowBitmap();
+	draw_text();
+}
+
+void CGameStateInit::load_background() {
+	background.LoadBitmapByString({ "Plants_vs_Zombies_Image/Scenes/Surface.bmp" });
+	background.SetTopLeft(0, 0);
+
+	start_button_1.LoadBitmapByString({ "Plants_vs_Zombies_Image/Scenes/SelectorScreenStartAdventure_1.bmp","Plants_vs_Zombies_Image/Scenes/SelectorScreenStartAdventure_2.bmp" }, RGB(255, 255, 255));
+	start_button_1.SetTopLeft(475, 75);
+}
+
+void CGameStateInit::draw_text() {
+	CDC *pDC = CDDraw::GetBackCDC();
+	/* Print title */
+
+	CTextDraw::ChangeFontLog(pDC,24, "微軟正黑體", RGB(255, 255, 255));
+	CTextDraw::Print(pDC, 0, 0, std::to_string(pointx));
+	CTextDraw::Print(pDC, 50, 0, std::to_string(pointy));
+
+	CDDraw::ReleaseBackCDC();
+}
+void CGameStateInit::OnMove() {
+	if (pointx >= 476 && pointx <= 797 && pointy >= 80 && pointy <= 188) {
+		start_button_1.SetFrameIndexOfBitmap(1); //SetFrameIndexOfBitmap選擇圖片
+	}
+	else {
+		start_button_1.SetFrameIndexOfBitmap(0);
+	}
+}
---
+++ LeistungsstarkesGameFramework-1.1.2/Source/Game/mygame_run.cpp
@@ -0,0 +1,97 @@
+#include "stdafx.h"
+#include "../Core/Resource.h"
+#include <mmsystem.h>
+#include <ddraw.h>
+#include "../Library/audio.h"
+#include "../Library/gameutil.h"
+#include "../Library/gamecore.h"
+#include "mygame.h"
+#include <string>
+
+using namespace game_framework;
+
+/////////////////////////////////////////////////////////////////////////////
+// 這個class為遊戲的遊戲執行物件，主要的遊戲程式都在這裡
+/////////////////////////////////////////////////////////////////////////////
+
+CGameStateRun::CGameStateRun(CGame *g) : CGameState(g)
+{
+}
+
+CGameStateRun::~CGameStateRun()
+{
+}
+
+void CGameStateRun::OnBeginState()
+{
+}
+
+void CGameStateRun::OnMove()							// 移動遊戲元素
+{
+	//遊戲剛開始的移動 往右到一定程度後 往回到原本樣式
+	if (fight_background.GetLeft() >= (-350) && BG1_flag1==0){
+		fight_background.SetTopLeft(fight_background.GetLeft() - 5, 0);
+		if (fight_background.GetLeft() <= (-350)) BG1_flag1 = 1;
+	}
+	else if (fight_background.GetLeft() <= (350) && BG1_flag1==1 ) {
+		fight_background.SetTopLeft(fight_background.GetLeft() + 5, 0);
+		if (fight_background.GetLeft() >= (350)) BG1_flag1 = 2;
+	}
+	
+
+}
+
+void CGameStateRun::OnInit()  								// 遊戲的初值及圖形設定
+{
+	fight_background.LoadBitmapByString({ "Plants_vs_Zombies_Image/Scenes/BG1.bmp" });
+	fight_background.SetTopLeft(0, 0);
+}
+
+void CGameStateRun::OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags)
+{
+	
+}
+
+void CGameStateRun::OnKeyUp(UINT nChar, UINT nRepCnt, UINT nFlags)
+{
+	
+}
+
+void CGameStateRun::OnLButtonDown(UINT nFlags, CPoint point)  // 處理滑鼠的動作
+{
+}
+
+void CGameStateRun::OnLButtonUp(UINT nFlags, CPoint point)	// 處理滑鼠的動作
+{
+}
+
+void CGameStateRun::OnMouseMove(UINT nFlags, CPoint point)	// 處理滑鼠的動作
+{
+	pointx = point.x;
+	pointy = point.y;
+}
+
+void CGameStateRun::OnRButtonDown(UINT nFlags, CPoint point)  // 處理滑鼠的動作
+{
+}
+
+void CGameStateRun::OnRButtonUp(UINT nFlags, CPoint point)	// 處理滑鼠的動作
+{
+}
+
+void CGameStateRun::OnShow()
+{
+	fight_background.ShowBitmap();
+	draw_text();
+}
+//-------------------------------------------------------------------------------------------
+void CGameStateRun::draw_text() {
+	CDC *pDC = CDDraw::GetBackCDC();
+	/* Print title */
+
+	CTextDraw::ChangeFontLog(pDC, 24, "微軟正黑體", RGB(255, 255, 255));
+	CTextDraw::Print(pDC, 0, 0, std::to_string(pointx));
+	CTextDraw::Print(pDC, 50, 0, std::to_string(pointy));
+
+	CDDraw::ReleaseBackCDC();
+}
