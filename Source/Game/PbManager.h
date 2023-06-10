766337d671c869734bfc26711fe20a8e9470656c
nonorze-VOXX <appieosugood@gmail.com>
2023-06-10 11:43:01 -0700

    move code out header

---
+++ Source/Game/PbManager.h
@@ -0,0 +1,7 @@
+#pragma once
+
+class PbManager
+{
+public:
+    
+};
--- Source/Game/PlantManager.cpp
+++ Source/Game/PlantManager.cpp
@@ -1,2 +1,98 @@
 #include "stdafx.h"
 #include "PlantManager.h"
+
+void PlantManager::setSunmanager(shared_ptr<SunManager> sm)
+{
+    sun_manager = sm;
+}
+
+void PlantManager::Update()
+{
+    int index=0;
+    vector<int> removeIndex;
+    for(auto& p:plants)
+    {
+        p->Update();
+        if(!p->GetActive())
+        {
+            removeIndex.insert(removeIndex.begin(),index);
+        }
+        index+=1;
+    }
+    for(auto& p:removeIndex)
+    {
+        plants.erase(plants.begin()+p);
+    }
+}
+
+void PlantManager::Show()
+{
+    for(auto& p:plants)
+    {
+        p->Show();
+    }
+}
+
+void PlantManager::OnMouseMove(Vector2 point)
+{
+    if(!plants.empty() && !plants.back()->GetIsPlace())
+    {
+        plants.back()->SetPositionByCenter(point);
+    }
+}
+
+void PlantManager::OnLButtonDown(Vector2 coordinate)
+{
+    if(!plants.empty() && !plants.back()->GetIsPlace())
+    {
+        plants.back()->SetCoordinate(coordinate);
+        plants.back()->SetTopLeft(
+            BoardLeft +BLOCK_WIDTH*static_cast<int>(coordinate.x),
+            BoardTop +BLOCK_HEIGHT*static_cast<int>(coordinate.y));
+        plants.back()->SetIsPlace(true);
+    }
+}
+
+void PlantManager::MakePlant(PlantType type, Vector2 position)
+{
+    if(!plants.empty()&&!plants.back()->GetIsPlace())
+    {
+        return;
+    }
+    //todo
+    std::shared_ptr<Plant> p;
+    switch (type)
+    {
+    case PlantType::SUN_FLOWER:
+        {
+            auto s=make_shared<Sunflower>();
+            s->Init();
+            s->setSunmanager(sun_manager);
+            s->SetAttackCounter(3000);
+            p=s;
+        }
+        break;
+    case PlantType::BEAN_PLANT: break;
+    case PlantType::NUT_PLANT: 
+        {
+            auto nut=make_shared<Nut>();
+            nut->Init();
+            p=nut;
+        }
+        break;
+    case PlantType::DOUBLE_BEAN: break;
+    default: ;
+    }
+    p->SetPositionByCenter(position);
+    plants.push_back(p);
+}
+
+void PlantManager::clear_plant()
+{
+    plants.clear();
+}
+
+int PlantManager::plant_size()
+{
+    return plants.size();
+}
